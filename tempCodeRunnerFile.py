cc028611290bbac9e1efa41bc1440f4748288207
Darshan Raykar <62028116+iamdarshan17@users.noreply.github.com>
2021-07-04 21:12:19 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,21 @@
+font = pygame.font.SysFont(None, 48)
+scorefont = pygame.font.SysFont(None, 30)
+
+fireimage = load_image('fire_bricks.png')
+firerect = fireimage.get_rect()
+
+cactusimage = load_image('cactus_bricks.png')
+cactusrect = cactusimage.get_rect()
+
+startimage = load_image('start.png')
+startimagerect = startimage.get_rect()
+startimagerect.centerx = window_width/2
+startimagerect.centery = window_height/2
+
+endimage = load_image('end.png')
+endimagerect = startimage.get_rect()
+endimagerect.centerx = window_width/2
+endimagerect.centery = window_height/2
+
+pygame.mixer.music.load('mario_theme.wav')
+gameover = pygame.mixer.Sound('mario_dies.wav')
